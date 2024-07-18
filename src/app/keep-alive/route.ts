ab322efb9f16f64b6adb2744dccfa595ad244950
shabeerp15 <shabeerp15@gmail.com>
2024-07-18 12:50:47 +0530

    added warm up cron job

---
+++ src/app/keep-alive/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+
+export const revalidate = 0
+export async function GET() {
+  await fetch(`https://${process.env.VERCEL_URL}/admin`)
+
+  return NextResponse.json({ ok: true })
+}
